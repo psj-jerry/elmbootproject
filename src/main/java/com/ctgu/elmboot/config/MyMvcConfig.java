4af20f7137141c85152d453656246011cac8a66f
JERRYLITTLE <745370327@qq.com>
2020-08-04 16:57:21 +0800

    注释测试

--- src/main/java/com/ctgu/elmboot/config/MyMvcConfig.java
+++ src/main/java/com/ctgu/elmboot/config/MyMvcConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+//注解
 @Configuration
 public class MyMvcConfig implements WebMvcConfigurer {
     @Bean
